adbc8f6b5e7bc57a5314e5d9e34063596737d854
Kalyan-24 <divyakalyan245@gmail.com>
2025-01-08 03:30:25 +0530

    .

--- server/routes/User.js
+++ server/routes/User.js
@@ -6,6 +6,10 @@ import isAuthenticated from "../middlewares/auth.js";
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    return res.status(200).json({ message: 'Hello World' })
+})
+
 router.post('/api/v1/holidays', postGetHolidaysController)
 
 router.post('/api/v1/get-user', isAuthenticated, postGetUserController)
